d4e447ff69199d9d3d4cc12902d61f262c61c0a9
Jerry <jerry.zf.he@viu.com>
2023-12-05 11:29:29 +0800

    Initial Commit

---
+++ Sources/MyLibrary/TestModel.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  SwiftPackageDemo
+//
+//  Created by gzteam on 2023/12/4.
+//
+
+import Foundation
+
+public struct TestModel: Codable {
+    
+    public var accessToken: String = ""
+    public var locale: String = ""
+    
+    public init() {
+        accessToken = "ABC"
+        locale = "v1.0.1"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        if let container = try? decoder.container(keyedBy: CodingKeys.self) {
+            accessToken = try! container.decode(String.self, forKey: .accessToken)
+            locale = try! container.decode(String.self, forKey: .locale)
+        }
+    }
+}
